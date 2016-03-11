83e7d112a15b36278fc0ffd42b2888ace666773e
Yannick Drolet <yannick.drolet@usherbrooke.ca>
2016-03-11 18:16:04 -0500

    Ajoute un .gitignore, un pom.xml et l'arborescence des dossiers de travail

---
+++ src/ClassePasRapport.java
@@ -0,0 +1,7 @@
+
+public class ClassePasRapport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world of the projet d'IFT232");
+    }
+}
---
+++ test/TestPasRapport.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+public class TestPasRapport {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Je suis un test");
+    }
+}
